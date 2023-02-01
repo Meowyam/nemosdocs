368117b411edb36f4b8aa78775a7154f1e683e7d
Meowyam <maryam@chilon.net>
2023-02-01 16:25:15 +0800

    updated with purple and relevant path conf

--- conf.py
+++ conf.py
@@ -6,31 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'L4'
-copyright = '2023, Meng Weng Wong'
-author = 'Meng Weng Wong, edited and organised by Nemo'
+project = "L4"
+copyright = "2023, Meng Weng Wong"
+author = "Meng Weng Wong, edited and organised by Nemo"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
-extensions = [
-    'sphinx_design' ,
-    'sphinxawesome_theme'
-]
+extensions = ["sphinx_design", "sphinxawesome_theme"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'pdf-version']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_themes", "_build", "Thumbs.db", ".DS_Store", "pdf-version"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinxawesome_theme'
-html_theme_path = ['../sphinxawesome-theme/src/sphinxawesome_theme']
-html_static_path = ['_static']
+html_theme = "sphinxawesome_theme"
+html_theme_path = ["_themes/sphinxawesome_theme/"]
+html_static_path = ["_static"]
 html_collapsible_definitions = True
